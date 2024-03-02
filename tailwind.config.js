45e7ceebaaeeeda8fd231b1ff1c44d7fe276c1b7
munawirnanang <muhamadmunawiramin@gmail.com>
2024-03-02 13:01:01 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+    fontSize: {
+      ssm: ['12px', '12px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    }
+  },
+  plugins: [
+    require("daisyui"),
+    require('flowbite/plugin')
+  ],
+}
+
